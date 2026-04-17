2b2df2258f5d11bc520b633c705566adb8c1f926
Frank Anthony Espino <skate_lp11@hotmail.com>
2026-04-17 18:54:03 -0500

    Modificando Foto y agregando tipado al titulo

--- src/app/features/hero/hero.component.ts
+++ src/app/features/hero/hero.component.ts
@@ -1,20 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.css'],
 })
-export class HeroComponent {
-  readonly meta = 'PORTAFOLIO 2026';
-  readonly signature = '+J / ROSE';
+export class HeroComponent implements OnInit {
+
   readonly title = 'ANTHONY ESPINO';
-  readonly subtitle = 'Full Stack Developer | Angular, Java, Spring Boot & Oracle';
-  readonly description = 'Desarrollo aplicaciones web conectando frontend y backend con APIs REST, manejo de bases de datos Oracle y seguridad con JWT. Experiencia con Java 8, Angular 14 y entornos empresariales.';
+  // readonly subtitle = 'Full Stack Developer | Angular, Java, Spring Boot & Oracle';
+  readonly description =
+    'Desarrollo aplicaciones web conectando frontend y backend con APIs REST, manejo de bases de datos Oracle y seguridad con JWT. Experiencia con Java, Angular y entornos empresariales.';
+
+  typedTitle = '';
+  private index = 0;
+
+  ngOnInit(): void {
+    this.startTyping();
+  }
+
+  startTyping(): void {
+    const speed = 80;
+
+    const typing = setInterval(() => {
+      if (this.index < this.title.length) {
+        this.typedTitle += this.title.charAt(this.index);
+        this.index++;
+      } else {
+        clearInterval(typing);
+      }
+    }, speed);
+  }
 
   scrollTo(id: string): void {
     const el = document.getElementById(id);
     el?.scrollIntoView({ behavior: 'smooth', block: 'start' });
   }
 }
-
