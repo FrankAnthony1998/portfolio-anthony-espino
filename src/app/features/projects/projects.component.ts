4e11052b5e6ff5d3f6dea7ac1267a5255f536254
Frank Anthony Espino <skate_lp11@hotmail.com>
2026-05-07 12:28:34 -0500

    Nueva demo de clinica estetica

--- src/app/features/projects/projects.component.ts
+++ src/app/features/projects/projects.component.ts
@@ -15,7 +15,7 @@ type Project = {
 export class ProjectsComponent {
   readonly projects: Project[] = [
     { title: 'DEMO E-COMMERCE', role: 'Full Stack', year: '2026', href: 'https://demo-ecommerce-next.netlify.app/' },
-    { title: 'API REST', role: 'Frontend', year: '2025', href: '' },
+    { title: 'DEMO BEAUTYTIME', role: 'Full Stack', year: '2026', href: 'https://beautytime-demo.netlify.app/' },
     { title: 'Proximamente...', role: 'Proximamente...', year: '2025', href: '' },
     { title: 'Proximamente...', role: 'Proximamente...', year: '2024', href: '' },
   ];
